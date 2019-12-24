b3649ab8016585c4abc5c4d1bc5ebe8d372fcaee
wangshibiao <645102170@qq.com>
2019-12-24 15:40:13 +0800

    feat(vue_webapp): 列表页支持下拉刷新

--- src/views/Film/FilmList.vue
+++ src/views/Film/FilmList.vue
@@ -1,57 +1,78 @@
 <template>
-  <list
-    :model="loading"
-    :finished="finished"
-    finished-text="没有更多了"
-    @load="onLoad"
-  >
-    <cell v-for="item in this.list" :key="item">
-      <div>hello</div>
-      <div>world</div>
-    </cell>
-  </list>
+  <div>
+    <pull-refresh v-model="isTopFreshLoading" @refresh="onTopFresh">
+      <list
+        :model="isBottomFreshLoading"
+        :finished="isBottomFreshFinished"
+        finished-text="没有更多了"
+        @load="onBottomFresh"
+      >
+        <cell v-for="item in this.list" :key="item">
+          <div>hello</div>
+          <div>world</div>
+        </cell>
+      </list>
+    </pull-refresh>
+  </div>
 </template>
 
 <script>
-import { List, Cell } from "vant";
+import { List, Cell, PullRefresh } from "vant";
 
 export default {
   name: "FilmList",
 
   components: {
     List,
-    Cell
+    Cell,
+    PullRefresh
   },
 
   data() {
     return {
+      // 列表页数据
       list: [],
-      loading: false,
-      finished: false
+
+      // 底部刷新相关
+      isBottomFreshLoading: false,
+      isBottomFreshFinished: false,
+
+      // 顶部刷新相关
+      isTopFreshLoading: false
     };
   },
 
   mounted() {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 2; i++) {
       this.list.push(i);
     }
   },
 
   methods: {
     /**
-     * 列表页组件滚动到底部
+     * 列表页底部刷新事件
      */
-    onLoad() {
-      console.log("onload", this.list.length);
+    onBottomFresh() {
+      console.log("onBottomFresh", this.list.length);
       this.list.push(this.list.length + 1);
 
       // 加载状态结束
-      this.loading = false;
+      this.isBottomFreshLoading = false;
 
       // 数据全部加载完成
       if (this.list.length >= 200) {
-        this.finished = true;
+        this.isBottomFreshFinished = true;
       }
+    },
+
+    /**
+     * 列表页顶部刷新事件
+     */
+    onTopFresh() {
+      console.log("onTopFresh", this.list.length);
+
+      this.list = [this.list.length + 1].concat(this.list);
+      this.isTopFreshLoading = false;
     }
   }
 };
