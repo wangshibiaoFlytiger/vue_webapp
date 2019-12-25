16bbebfabe8c205a129e933cc701af2d6f2348fc
wangshibiao <645102170@qq.com>
2019-12-25 18:39:09 +0800

    feat(vue_webapp): 定义FilmListItem组件, 同时使用film模拟数据
    FilmListItem组件使用vant布局组件

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import { List, Cell, PullRefresh } from "vant";
+import { List, Cell, PullRefresh, Row, Col } from "vant";
 
 // 注册全局组件, 这样就无需在使用组件之前前做import操作了
 Vue.use(List)
   .use(Cell)
-  .use(PullRefresh);
+  .use(PullRefresh)
+  .use(Row)
+  .use(Col);
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Film/FilmListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <van-row>
+        <van-col span="12">{{ item.title }}</van-col>
+        <van-col span="12">{{ item.category }}</van-col>
+      </van-row>
+
+      <van-row>
+        <img :src="item.coverUrl" />
+      </van-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilmListItem",
+
+  props: {
+    item: Object
+  },
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+
+<style scoped></style>
